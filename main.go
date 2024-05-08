99385e12c36e9443d3eb39c0e5a008405d69f8ad
polyhistor2050 <chrisspaul2050@gmail.com>
2024-05-08 23:08:59 +0300

    Define handler as a separate package

--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
 	"net/http"
 )
 
 func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello, world!!")
-
-		data, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "An error occured", http.StatusBadRequest)
-			return
-		}
-		log.Printf("Data %s\n", data)
-
-		// write back to the user
-		fmt.Fprintf(w, "Hello %s\n", data)
-	})
-
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye world!!")
-	})
 
 	http.ListenAndServe(":8080", nil)
 }
