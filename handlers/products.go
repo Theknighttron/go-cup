6006fb319bb2d6ad4eae4eea75732be51c01c5a9
polyhistor2050 <chrisspaul2050@gmail.com>
2024-05-18 12:12:56 +0300

    Updates

--- handlers/products.go
+++ handlers/products.go
@@ -16,11 +16,21 @@ func NewProduct(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodGet {
+        p.getProducts(rw, r)
+        return
+    }
+
+    rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+    // Return a list of all products
     lp := data.GetProducts()
     err := lp.ToJSON(rw)
 
     if err != nil {
         http.Error(rw, "Unable to marshal", http.StatusInternalServerError)
     }
-
 }
